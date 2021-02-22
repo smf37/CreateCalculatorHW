acb072242e68955c315955f245a020d350f52d4a
stephen fusco <smf37@njit.edu>
2021-02-22 17:11:35 -0500

    Task: made csv reader its own folder

--- csvReader/csv_Reader.py
+++ csvReader/csv_Reader.py
@@ -1,3 +1,24 @@
 import csv
 from pathlib import Path
 
+def absolute_path(filepath):
+    relative = Path(filepath)
+    return relative.absolute()
+
+
+class Reader:
+    content = []
+
+    def __init__(self, file):
+        self.content = []
+        self.reader(file)
+
+    def reader(self, path):
+        with open(absolute_path(path)) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                if '' in row:
+                    row.remove('')
+                self.content.append(row)
+
+
