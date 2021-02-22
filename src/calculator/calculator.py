e7d6ab30d5fc2cdc51be5e74d92a3451a0565c09
stephen fusco <smf37@njit.edu>
2021-02-22 17:10:54 -0500

    Feature: Refractor for travis

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,5 +1,31 @@
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = Addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = Subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = Multiplication(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = Division(a, b)
+        return self.result
+
+    def square(self, a, b):
+        self.result = Square(a, b)
+        return self.result
+
+    def squareRoot(self, a, b):
+        self.result = SquareRoot(a, b)
+        return self.result
