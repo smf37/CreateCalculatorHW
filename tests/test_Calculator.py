6ad8c92f21a12e9cc4ee3c27341aad681095b618
stephen fusco <smf37@njit.edu>
2021-02-22 17:54:38 -0500

    Feature: added methods to help run class

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -14,12 +14,48 @@ class CalculatorTestCase(unittest.TestCase):
     def test_result_is_zero_calculator(self):
         self.assertEqual(self.calculator.result, 0)
 
-    def test_addition(self):
+    def test_Addition(self):
         file = Reader("testCases/Unit_Test_Addition.csv").content
-        print("test addition")
+        print("test addition\n")
         for row in file:
             result = float(row[2])
             self.assertEqual(self.calculator.add(row[0], row[1]), float(row[2]))
+
+    def test_Subtraction(self):
+        file = Reader("testCases/Unit_Test_Subtraction.csv").content
+        print("test subtraction\n")
+        for row in file:
+            result = float(row[2])
+            self.assertEqual(self.calculator.add(row[0], row[1]), float(row[2]))
+
+    def test_Division(self):
+        file = Reader("testCases/Unit_Test_Division.csv").content
+        print("test division\n")
+        for row in file:
+            result = float(row[2])
+            self.assertEqual(self.calculator.division(row[0], row[1]), float(row[2]))
+
+    def test_Multiplication(self):
+        file = Reader("testCases/Unit_Test_Multiplication.csv").content
+        print("test multiplication\n")
+        for row in file:
+            result = float(row[2])
+            self.assertEqual(self.calculator.multiply(row[0], row[1]), float(row[2]))
+
+    def test_Square(self):
+        file = Reader("testCases/Unit_Test_Square.csv").content
+        print("test square\n")
+        for row in file:
+            result = float(row[1])
+            self.assertEqual(self.calculator.square(row[0]), float(row[1]))
+
+    def test_Square_Root(self):
+        file = Reader("testCases/Unit_Test_Square_Root.csv").content
+        print("test square root\n")
+        for row in file:
+            result = float(row[1])
+            self.assertEqual(self.calculator.squareRoot(row[0]), float(row[1]))
+            self.assertAlmostEqual(self.calculator.result, result)
 
 
 
