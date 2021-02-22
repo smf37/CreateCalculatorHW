ebe7b404e99736dda5e60728b0f341a23b94fa3a
stephen fusco <smf37@njit.edu>
2021-02-22 17:11:55 -0500

    Fix: updated paths

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.calculator.calculator import Calculator
+from csvReader.csv_Reader import Reader
 
 
 class CalculatorTestCase(unittest.TestCase):
@@ -12,6 +13,17 @@ class CalculatorTestCase(unittest.TestCase):
 
     def test_result_is_zero_calculator(self):
         self.assertEqual(self.calculator.result, 0)
+
+    def test_addition(self):
+        file = Reader("testCases/Unit_Test_Addition.csv").content
+        print("test addition")
+        for row in file:
+            result = float(row[2])
+            self.assertEqual(self.calculator.add(row[0], row[1]), float(row[2]))
+
+
+
+
 
 
 
