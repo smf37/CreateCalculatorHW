2172fa3edae42696a8f8ed85cabf58010ae3aecb
stephen fusco <smf37@njit.edu>
2021-02-28 20:35:19 -0500

    Feature: Used functions.py file to call the static methods.

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,9 +1,10 @@
-from src.calculator.Addition import Addition
-from src.calculator.Subtraction import Subtraction
-from src.calculator.Division import Division
-from src.calculator.Multiplication import Multiplication
-from src.calculator.Square import Square
-from src.calculator.SquareRoot import Square_Root
+from src.functions.functions import Functions
+#from src.calculator.Addition import Addition
+#from src.calculator.Subtraction import Subtraction
+#from src.calculator.Division import Division
+#from src.calculator.Multiplication import Multiplication
+#from src.calculator.Square import Square
+#from src.calculator.SquareRoot import Square_Root
 
 class Calculator:
     result = 0
@@ -12,25 +13,25 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = Addition(a, b)
+        self.result = Functions.Addition(a, b)
         return self.result
 
     def subtract(self, a, b):
-        self.result = Subtraction(a, b)
+        self.result = Functions.Subtraction(a, b)
         return self.result
 
     def multiply(self, a, b):
-        self.result = Multiplication(a, b)
+        self.result = Functions.Multiplication(a, b)
         return self.result
 
     def division(self, a, b):
-        self.result = round(Division(a, b), 9)
+        self.result = round(Functions.Division(a, b), 9)
         return self.result
 
     def square(self, a):
-        self.result = Square(a)
+        self.result = Functions.Square(a)
         return self.result
 
     def squareRoot(self, a):
-        self.result = Square_Root(a)
+        self.result = Functions.Square_Root(a)
         return self.result
